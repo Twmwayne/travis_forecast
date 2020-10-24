419c89d109e4f548f9ef4b139939d70a3cf9bd03
Travis Mendenhall <twmwayne@gmail.com>
2020-10-24 08:48:36 -0600

    search history function, and render search history js

--- script.js
+++ script.js
@@ -80,7 +80,7 @@ function initPage() {
     }
 
     searchEl.addEventListener("click",function() {
-        const searchTerm = inputEl.nodeValue;
+        const searchTerm = inputEl.value;
         getWeather(searchTerm);
         searchHistory.push(searchTerm);
         localStorage.setItem("search",JSON.stringify(searchHistory));
@@ -91,6 +91,32 @@ function initPage() {
         searchHistory = [];
         renderSearchHistory();
     })
+ 
+    function k2f(K) {
+        return Math.floor((K - 273.15) *1.8 + 32);
+    }
+
+    function renderSearchHistory() {
+        historyEl.innerHTML = "";
+        for (let i=0; i < searchHistory.length; i++) {
+            const historyItem = document.createElement("input");
+            historyItem.setAttribute("type","text");
+            historyItem.setAttribute("readonly",true);
+            historyItem.setAttribute("class","form-control d-block bg-white");
+            historyItem.setAttribute("value", searchHistory[i]);
+            historyItem.addEventListener("click",function() {
+                getWeather(historyItem.value);
+            })
+            historyEl.append(historyItem);
+        }
+
+    }
+
+    renderSearchHistory();
+    if (searchHistory.length > 0) {
+        getWeather(searchHistory[searchHistory.length - 1]);
+    }
 
 
 }
+initPage();
